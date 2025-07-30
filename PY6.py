6db21c6aff55b8ad31322195c9a17afb50308383
santhiya <sandykumar2000626wwww@gmail.com>
2025-07-30 15:51:27 +0530

    Add files via upload

---
+++ PY6.py
@@ -0,0 +1,14 @@
+print("Square and rectangle")
+print("-------------")
+a=int(input("Enter the a value:"))
+b=int(input("Enter the b value:"))
+l=int(input("Enter the l value:"))
+print("RESULT")
+ps=4*a
+print("Perimetere of Square:",ps)
+ass=a*a
+print("Area of Square:",ass)
+pr=2*(l+b)
+print("Perimeter of Rectangle:",pr)
+ar=l*b
+print("Area of Rectangle:",ar)
---
+++ PY7.py
@@ -0,0 +1,11 @@
+print("Traingle Program")
+print("---------------")
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+c=int(input("Enter the c value"))
+h=int(input("Enter the h value"))
+print("RESULT")
+pt=a+b+c
+print("Perimeter of Traingle:",pt)
+at=1/2*b*h
+print("Area of Traingle:",at)
